d3590631baf33623f9ac85a4890f6dff3176c440
Kari Gilbertson <kari.a.gilbertson@gmail.com>
2017-02-23 01:19:26 -0500

    Commenting Clean Up

--- scripts/switch.js
+++ scripts/switch.js
@@ -1,3 +1,11 @@
+/* This function is to swap the different style sheets out.
+There is a sheet for:
+	the original style,
+	contrast style,
+	enlargement style,
+	contrast & enlargement style
+*/
+
 function swapSheet() {
 	if(document.getElementById("contrast-switch").checked == true && document.getElementById("text-switch").checked == true)
 		document.getElementById('pagestyle').setAttribute('href', 'styles/both.css');
